a1dd9471e80ce9e1c6b2a7053177477e0f3501b6
Viktor Bibik <viktorbibik1988@gmail.com>
2023-02-13 21:56:38 +0200

    lesson_26_homework_21

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -5,27 +5,21 @@ import my.exceptions.LessonDoesNotExistsException;
 import my.exceptions.LessonsArrayIsEmptyException;
 import repo.LessonDao;
 
+
 public class Application {
     public static void main(String[] args) {
-        try(DataBaseConnection mySqlConnection = new DataBaseConnection()){
+        try(DataBaseConnection mySqlConnection = DataBaseConnection.getInstance()){
             LessonDao lessonDao = new LessonDao(DataBaseConnection.getConnection());
             lessonDao.getAllLessons();
             lessonDao.getLessonById(2);
-//            lessonDao.getLessonById(3);
             lessonDao.deleteLesson(lessonDao.getLastAddedLessonId());
             lessonDao.addLesson("Math", "2022-01-01 12:45:36", 3);
             lessonDao.getAllLessons();
-
-//        mySqlConnection.close(DataBaseConnection.getConnection());
         } catch (DBCustomeException | LessonDoesNotExistsException | LessonsArrayIsEmptyException | DublicatedHomeWorkIDException e) {
             System.out.println(e.getMessage());
-//            e.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
-        System.out.println();
     }
 }
 
--- src/main/java/dbconnection/DataBaseConnection.java
+++ src/main/java/dbconnection/DataBaseConnection.java
@@ -3,39 +3,35 @@ package dbconnection;
 import java.sql.*;
 
 public class DataBaseConnection implements AutoCloseable {
+    private static DataBaseConnection dataBaseConnection;
     private static Connection connection;
 
     private static final String url = "jdbc:mysql://localhost:3306/test_mysql_db";
     private static final String username = System.getenv("MYSQL_USER");
     private static final String password = System.getenv("MYSQL_PASSWORD");
 
-//    public Connection getConnection() {
-//        try{
-//            Class.forName("com.mysql.cj.jdbc.Driver");
-//
-//            connection = DriverManager.getConnection(url, username, password);
-//
-//        } catch (ClassNotFoundException | SQLException e) {
-//            e.printStackTrace();
-//        }
-//        return connection;
-//    }
+    private DataBaseConnection() {
+    }
+
+    public static DataBaseConnection getInstance() {
+        if (dataBaseConnection == null)
+            dataBaseConnection = new DataBaseConnection();
+        return dataBaseConnection;
+    }
 
     public static Connection getConnection() {
         try {
-            if (connection == null){
+            if (connection == null) {
                 Class.forName("com.mysql.cj.jdbc.Driver");
                 connection = DriverManager.getConnection(url, username, password);
             }
-
-
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
         return connection;
     }
 
-    public void close (Connection connection){
+    public void close(Connection connection) {
         try {
             connection.close();
         } catch (SQLException throwables) {
