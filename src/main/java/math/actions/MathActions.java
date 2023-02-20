d0f351140e322b833703e5b374e49ecef53ca2e8
Viktor Bibik <viktorbibik1988@gmail.com>
2023-02-20 22:55:44 +0200

    add/test-methods

--- src/main/java/math/actions/MathActions.java
+++ src/main/java/math/actions/MathActions.java
@@ -1,11 +1,41 @@
 package math.actions;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
+
 public class MathActions {
-    public int multiply(int a, int b){
-        return Math.multiplyExact(a, b);
+    public double multiply(double a, double b){
+        return a * b;
     }
 
     public double divide(double a, double b){
         return a / b;
+    }
+
+    public double subtract(double a, double b){
+        return a - b;
+    }
+
+    public double sum(double a, double b){
+        return a + b;
+    }
+
+    public <T extends Double> double max(T[] arr){
+        List<T> listOfDoubles = Arrays.asList(arr);
+
+        return listOfDoubles.stream()
+                .mapToDouble(d -> d)
+                .max().orElseThrow(NoSuchElementException::new);
+    }
+
+
+    public <T extends Double> double min(T[] arr){
+        List<T> listOfDoubles = Arrays.asList(arr);
+
+        return listOfDoubles.stream()
+                .mapToDouble(d -> d)
+                .min().orElseThrow(NoSuchElementException::new);
     }
 }
--- src/test/java/meth/actions/MathActionsTest.java
+++ src/test/java/meth/actions/MathActionsTest.java
@@ -16,11 +16,11 @@ public class MathActionsTest {
 
     @Test
     public void multiply_success(){
-        int a = 10;
-        int b = 2;
+        double a = 10;
+        double b = 2;
 
-        int actual = mathActions.multiply(a, b);
-        int expected = 20;
+        double actual = mathActions.multiply(a, b);
+        double expected = 20;
 
         assertEquals(expected, actual);
     }
@@ -32,6 +32,48 @@ public class MathActionsTest {
 
         double actual = mathActions.divide(a, b);
         double expected = 5;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void subtract_success(){
+        double a = 10;
+        double b = 2;
+
+        double actual = mathActions.subtract(a, b);
+        double expected = 8;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sum_success(){
+        double a = 10;
+        double b = 2;
+
+        double actual = mathActions.sum(a, b);
+        double expected = 12;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void max_success(){
+        Double[] arr = {1.0, 5.5, 45.8, 32.5, 5.4, 76.1, 4.8, -154.8};
+
+        double actual = mathActions.max(arr);
+        double expected = 76.1;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void min_success(){
+        Double[] arr = {17.8, 5.5, 45.8, 32.5, 5.4, 76.1, 4.8, -154.8};
+
+        double actual = mathActions.min(arr);
+        double expected = -154.8;
 
         assertEquals(expected, actual);
     }
