f70b1cfc588805ce33d1ff353cae1bb126ff1dab
Viktor Bibik <viktorbibik1988@gmail.com>
2023-03-09 22:52:51 +0200

    add/write_consoleLog_to_the_file_and_to_console

--- src/main/java/runner/ProjectTestRunner.java
+++ src/main/java/runner/ProjectTestRunner.java
@@ -6,73 +6,43 @@ import java.io.*;
 
 public class ProjectTestRunner {
     public void runByPackageName(String packageName){
-        PrintStream print = System.out;
-        PrintStream printErr = System.err;
-        ConsoleLauncher.execute(print, printErr, String.format("-p %s", packageName));
-
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(String.format("%s.txt", packageName), true))) {
-            PrintWriter printWriter = new PrintWriter(bufferedWriter);
-            ConsoleLauncher.execute(printWriter, printWriter, String.format("-p %s", packageName));
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to write message", e);
-        }
+        runConsoleAndWriteResult(packageName, "-p %s");
     }
 
     public void runByClassName(String className){
-        PrintStream print = System.out;
-        PrintStream printErr = System.err;
-
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(String.format("%s.txt", className), true))) {
-            PrintWriter printWriter = new PrintWriter(bufferedWriter);
-            ConsoleLauncher.execute(printWriter, printWriter, String.format("-c %s", className));
-            ConsoleLauncher.execute(print, printErr, String.format("-c %s", className));
-
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to write message", e);
-        }
+        runConsoleAndWriteResult(className, "-c %s");
     }
 
     public void runByClass(Class<?> aClass) {
-        PrintStream print = System.out;
-        PrintStream printErr = System.err;
-        ConsoleLauncher.execute(print, printErr, String.format("-c %s", aClass.getName()));
-
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(String.format("%s.txt", aClass.getName()), true))) {
-            PrintWriter printWriter = new PrintWriter(bufferedWriter);
-            ConsoleLauncher.execute(printWriter, printWriter, String.format("-c %s", aClass.getName()));
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to write message", e);
-        }
+        runConsoleAndWriteResult(aClass.getName(), "-c %s");
     }
 
     public void runByClasses(Class<?>... aClass) {
         for (Class bClass: aClass) {
-            PrintStream print = System.out;
-            PrintStream printErr = System.err;
-
-            ConsoleLauncher.execute(print, printErr, String.format("-c %s", bClass.getName()));
-
-            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(String.format("%s.txt", bClass.getName()), true))) {
-                PrintWriter printWriter = new PrintWriter(bufferedWriter);
-                ConsoleLauncher.execute(printWriter, printWriter, String.format("-c %s", bClass.getName()));
-            } catch (IOException e) {
-                throw new RuntimeException("Failed to write message", e);
-            }
+            runConsoleAndWriteResult(bClass.getName(), "-c %s");
         }
     }
     public void runByClassesNames(String... className) {
         for (String bClass: className) {
-            PrintStream print = System.out;
-            PrintStream printErr = System.err;
-
-            ConsoleLauncher.execute(print, printErr, String.format("-c %s", bClass));
-
-            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(String.format("%s.txt", bClass), true))) {
-                PrintWriter printWriter = new PrintWriter(bufferedWriter);
-                ConsoleLauncher.execute(printWriter, printWriter, String.format("-c %s", bClass));
-            } catch (IOException e) {
-                throw new RuntimeException("Failed to write message", e);
-            }
+            runConsoleAndWriteResult(bClass, "-c %s");
         }
+    }
+
+    private void runConsoleAndWriteResult(String coreName, String s) {
+        ByteArrayOutputStream bout = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(bout);
+        ConsoleLauncher.execute(printStream, printStream, String.format(s, coreName));
+
+        String resultString = bout.toString();
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(String.format("%s.txt", coreName)))) {
+            PrintWriter printWriter = new PrintWriter(bufferedWriter);
+            printWriter.println(resultString);
+
+            printWriter.flush();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to write message", e);
+        }
+        System.out.println(resultString);
     }
 }
